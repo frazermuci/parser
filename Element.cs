f51371b4791eb361013beee4a9413134badeb48d
MATTHEWFRAZER <mfrazeriguess@gmail.com>
2017-02-25 13:22:33 -0800

    added element struct

---
+++ Element.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    struct Element
+    {
+
+        public bool isText;
+        public string data;
+        public string name;
+        public Element(bool isText, string data, string name)
+        {
+            this.isText = isText;
+            this.data = data;
+            this.name = name;
+        }
+    }
+}
